e91a33b7daacde78da2d0c4b27850e7f9de04a74
Samuel Horjet <samuelhorjet001@gmail.com>
2025-12-24 15:14:54 +0000

    Remove output configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
     images: {
         remotePatterns: [
             {
